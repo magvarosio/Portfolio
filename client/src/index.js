1f78af2344f1643c2d9305fbcbc558cbcc2f44ba
“Mag <margherita.varosio@gmail.com>
2023-01-09 17:52:12 +0000

    Deploy

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createRoot } from 'react-dom/client'
+import './styles/main.scss'
+import './fonts/sysfont.woff'
 
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 // import 'mdbreact/dist/css/mdb.css'
 // import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 
